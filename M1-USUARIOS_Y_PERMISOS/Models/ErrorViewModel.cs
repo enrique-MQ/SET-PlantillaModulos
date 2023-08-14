14f25a27af1e15ecfaf6f41a8a31cecf6f3cf8f2
Enrique Martinez <enriquemartinez.qz@gmail.com>
2023-08-14 08:47:35 -0600

    Plantilla - Primera Version - SIAN

---
+++ M1-USUARIOS_Y_PERMISOS/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace M1_USUARIOS_Y_PERMISOS.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
